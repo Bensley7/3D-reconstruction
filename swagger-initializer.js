586f7249a09804e323c11c6c2cb74e1362616e5f
mob <mob@beefutures.io>
2025-02-02 23:26:07 +0100

    change url

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://github.com/Bensley7/3D-reconstruction/main/openapi.yaml",
+    url: "https://github.com/Bensley7/3D-reconstruction/master/openapi.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
